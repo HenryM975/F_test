32beee3a3432237bdb0ff65a202c4ef955b7a645
HenryM975 <henry975@yandex.ru>
2020-07-05 16:58:34 +0300

    errors

--- fmain.py
+++ fmain.py
@@ -9,6 +9,12 @@ def index():
     #return '<h1> smth <h1>'
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+
 
     #@app.route('/index')
     #def findex():
