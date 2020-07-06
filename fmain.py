b23f9627682c231d45fe17a6c55c6f495ffbc2f2
HenryM975 <henry975@yandex.ru>
2020-07-06 16:00:49 +0300

    pages

--- fmain.py
+++ fmain.py
@@ -2,12 +2,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
 #try:
 @app.route('/')
 def index():
     #return '<h1> smth <h1>'
     return render_template('index.html')
+
+@app.route('/link1')
+def link_1():
+    #return '<h1> smth <h1>'
+    return render_template('link_1.html')
+
+@app.route('/link2')
+def link_2():
+    #return '<h1> smth <h1>'
+    return render_template('link_2.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
