2a73220b819d240a87c76d56bbb6266474f3d040
HenryM975 <henry975@yandex.ru>
2020-07-07 23:28:53 +0300

    reg?

--- fmain.py
+++ fmain.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from flask_wtf import Form #
+from wtforms import StringField, SubmitField #
+from wtforms.validators import Required #
 app = Flask(__name__)
 
 
@@ -18,12 +21,19 @@ def link_2():
     #return '<h1> smth <h1>'
     return render_template('link_2.html')
 
+@app.route('/reg')
+def reg():
+    #return '<h1> smth <h1>'
+    return render_template('reg.html')
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
-
-
-
+#>? work with reg.html
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+#<
 
     #@app.route('/index')
     #def findex():
